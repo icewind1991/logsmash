d0997370bb3166eb8d29ebb862263ef5d59c9518
Robin Appelman <robin@icewind.nl>
2024-07-28 18:58:51 +0200

    jemalloc

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,13 @@ mod matcher;
 mod timegraph;
 mod ui;
 
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 #[derive(Debug, Parser)]
 struct Args {
     file: String,
